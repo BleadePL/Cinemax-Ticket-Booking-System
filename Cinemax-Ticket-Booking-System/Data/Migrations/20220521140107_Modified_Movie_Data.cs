ff81946c75483c10103d2df5723776782c2bae4c
Konrad <konrad.wieckiewicz@gmail.com>
2022-05-21 17:05:55 +0200

    -Added ScreenPatter Field

---
+++ Cinemax-Ticket-Booking-System/Data/Migrations/20220521140107_Modified_Movie_Data.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cinemax_Ticket_Booking_System.Data.Migrations
+{
+    public partial class Modified_Movie_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Movie");
+        }
+    }
+}
---
+++ Cinemax-Ticket-Booking-System/Data/Migrations/20220521143953_ScreenRoom_Pattern.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cinemax_Ticket_Booking_System.Data.Migrations
+{
+    public partial class ScreenRoom_Pattern : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ScreenPattern",
+                table: "ScreeningRoom",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScreenPattern",
+                table: "ScreeningRoom");
+        }
+    }
+}
