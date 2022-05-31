db10a3307812a87b6a98a63883a04406c6fcf8df
Pawel Bytner <pawelbytner.dev@gmail.com>
2022-05-31 19:44:14 +0200

    added script to get row and col of selected seat

--- Cinemax-Ticket-Booking-System/wwwroot/js/tmp.js
+++ Cinemax-Ticket-Booking-System/wwwroot/js/tmp.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $(".room-seats__seat").click(function() {
+    $(this).addClass("choosen");
     $(this).css('background-image', 'url("../../image/seat_choosen.svg")');
+    let colChoosen = $(this).find(".room-seats__seat-number").text();
+    let rowChoosen = $(this).parent().find(".room-seats__row-number").text();
   });
 });
