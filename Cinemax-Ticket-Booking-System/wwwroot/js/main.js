8064ba22777d52126b73aa68ebd79cf4a29b0ab0
Pawel Bytner <pawelbytner.dev@gmail.com>
2022-06-04 21:54:00 +0200

    added cookies for reservation

--- Cinemax-Ticket-Booking-System/wwwroot/js/main.js
+++ Cinemax-Ticket-Booking-System/wwwroot/js/main.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
+  var colChoosen;
+  var rowChoosen;
+  var showId;
+  var isPurchased;
+
+  function SetCookie(c_name,value,expiredays) {
+		const exdate=new Date()
+		exdate.setDate(exdate.getDate()+expiredays)
+		document.cookie=c_name+ "=" + value +
+		((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+	};
+
   $(".room-seats__seat").click(function() {
     $(this).addClass("choosen");
     $(this).css('background-image', 'url("../../image/seat_choosen.svg")');
-    let colChoosen = $(this).find(".room-seats__seat-number").text();
-    let rowChoosen = $(this).parent().find(".room-seats__row-number").text();
+    colChoosen = $(this).find(".room-seats__seat-number").text();
+    rowChoosen = $(this).parent().find(".room-seats__row-number").text();
+    showId = $(".room-seats__showId").text();
+    isPurchased = true;
 
-    console.log(`${colChoosen}, ${rowChoosen}`);
+    if($(this).find(".seat-reservation-hidden-value").text() === "-1") {
+      isPurchased = false;
+    };
+  });
+
+  $(".testAction").click(function() {
+    SetCookie("Reservation", `[{showId:${showId}, row:${rowChoosen}, col:${colChoosen}, isPurchased:${isPurchased}}]`, 1);
   });
 });
